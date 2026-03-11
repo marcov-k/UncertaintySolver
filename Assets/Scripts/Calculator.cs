1791b75066b73532ee9bea5dba231d030fb5a30b
Marco Vasko-Klima <marcoaklima@gmail.com>
2026-03-11 22:17:26 +0100

    Added basic input system and input validation

---
+++ Assets/Scripts/Calculator.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using TMPro;
+
+public class Calculator : MonoBehaviour
+{
+    [SerializeField] TMP_InputField userInput;
+    [SerializeField] TMP_InputField outputField;
+
+    public void Calculate()
+    {
+
+    }
+}
---
+++ Assets/Scripts/InputValidator.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using TMPro;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+[CreateAssetMenu(fileName = "New Input Validator", menuName = "Input Validator")]
+public class InputValidator : TMP_InputValidator
+{
+    [SerializeField] bool AllowNumbers;
+    [SerializeField] bool AllowLetters;
+    [SerializeField] List<char> AllowedCharacters = new();
+    [SerializeField] string MatchRegex = string.Empty;
+    Regex regex = null;
+
+    void OnEnable()
+    {
+        if (!string.IsNullOrEmpty(MatchRegex))
+        {
+            regex = new(MatchRegex);
+        }
+        else regex = null;
+    }
+
+    public override char Validate(ref string text, ref int pos, char ch)
+    {
+        if ((AllowNumbers && char.IsNumber(ch)) || (AllowLetters && char.IsLetter(ch)) || AllowedCharacters.Contains(ch))
+        {
+            string newText = text + ch;
+            if (regex != null && !regex.IsMatch(newText))
+            {
+                return '\0';
+            }
+            text = newText;
+            pos++;
+            return ch;
+        }
+        else return '\0';
+    }
+}
